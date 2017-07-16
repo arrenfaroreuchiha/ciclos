8cdbc8a68be0a0d2f90ebc679be7dd1e9ae03bd4
Eduardo <eduardosr130@gmail.com>
2017-07-16 17:56:15 -0500

    nuevo

---
+++ 13.py
@@ -0,0 +1,16 @@
+cantidad = int(raw_input("cantidad:"))
+matrix = []
+
+for i in range(cantidad):
+	matrix.append([])
+	for x in range(cantidad):
+		numero = int(raw_input("Numero:"))
+		matrix[i].append(numero)
+
+print matrix
+
+vector = []
+for i in range(cantidad):
+	print vector.insert(i, numero)
+
+
